852d3480d3a846e3f7861e70b44ab5f333bdffe7
djandrade <djandrade@outlook.com>
2017-11-08 23:36:57 -0500

    Avance Desarrollo experiencia docente
    Avance Desarrollo experiencia docente

---
+++ Convocatoria.Web.Front/Models/ExperienciaDocenteModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Convocatoria.Web.Front.Models
+{
+    public class ExperienciaDocenteModel: AuditoriaModel
+    {
+        public int Id { get; set; }
+
+        public int IdHojaVida { get; set; }
+
+        public DateTime? FechaIngreso { get; set; }
+
+        public int IdEntidadEducativa { get; set; }
+
+        public int IdTipoVinculacion { get; set; }
+
+        public int? HorasMensuales { get; set; }
+
+        public string TelefonoEntidadEducativa { get; set; }
+
+        public string DireccionEntidadEducativa { get; set; }
+
+        public int IdCiudadEntidadEducativa { get; set; }
+
+        public DateTime? FechaTerminacion { get; set; }
+    }
+}
---
+++ Convoctaoria.Negocio/Dtos/Docente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Convocatoria.Negocio.StandarContract;
+
+namespace Convocatoria.Negocio.Dtos
+{
+    public class Docente: ContractBll
+    {
+        public int Id { get; set; }
+
+        public int IdHojaVida { get; set; }
+
+        public DateTime? FechaIngreso { get; set; }
+
+        public int IdEntidadEducativa { get; set; }
+
+        public int IdTipoVinculacion { get; set; }
+
+        public int? HorasMensuales { get; set; }
+
+        public string TelefonoEntidadEducativa { get; set; }
+
+        public string DireccionEntidadEducativa { get; set; }
+
+        public int IdCiudadEntidadEducativa { get; set; }
+
+        public DateTime? FechaTerminacion { get; set; }
+    }
+}
